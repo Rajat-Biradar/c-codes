a2d2e50ad9242264fd725c1ca8865b94c032a58d
Rajat-Biradar <rajatbiradar37@gmail.com>
2023-10-14 20:56:06 +0530

    first commit

---
+++ .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+    { int a,b,sum;
+    
+    printf("enter the numb a,b:");
+    scanf("%d%d",a,&b);
+    sum=a+b;
+    printf("\na=%d,&b=%d ,& sum=%d",sum);
+    return(0);
+    }
+    
+    
